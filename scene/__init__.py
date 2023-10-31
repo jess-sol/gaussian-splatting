79724c20e6a995880271218d61fcf73abe2cc76f
Jess <jess@ros.io>
2023-10-31 07:31:39 -0600

    Add support for reading from e57 files

--- scene/__init__.py
+++ scene/__init__.py
@@ -40,7 +40,9 @@ class Scene:
         self.train_cameras = {}
         self.test_cameras = {}
 
-        if os.path.exists(os.path.join(args.source_path, "sparse")):
+        if args.source_path.endswith(".e57"):
+            scene_info = sceneLoadTypeCallbacks["E57"](args.source_path, args.eval)
+        elif os.path.exists(os.path.join(args.source_path, "sparse")):
             scene_info = sceneLoadTypeCallbacks["Colmap"](args.source_path, args.images, args.eval)
         elif os.path.exists(os.path.join(args.source_path, "transforms_train.json")):
             print("Found transforms_train.json file, assuming Blender data set!")
--- scene/dataset_readers.py
+++ scene/dataset_readers.py
@@ -10,6 +10,7 @@
 #
 
 import os
+import io
 import sys
 from PIL import Image
 from typing import NamedTuple
@@ -117,7 +118,7 @@ def storePly(path, xyz, rgb):
     dtype = [('x', 'f4'), ('y', 'f4'), ('z', 'f4'),
             ('nx', 'f4'), ('ny', 'f4'), ('nz', 'f4'),
             ('red', 'u1'), ('green', 'u1'), ('blue', 'u1')]
-    
+
     normals = np.zeros_like(xyz)
 
     elements = np.empty(xyz.shape[0], dtype=dtype)
@@ -254,7 +255,189 @@ def readNerfSyntheticInfo(path, white_background, eval, extension=".png"):
                            ply_path=ply_path)
     return scene_info
 
+
+def readE57SceneInfo(path, eval, llffhold=8):
+    import pye57
+    import quaternion
+    from tqdm import tqdm
+    import open3d as o3d
+    from collections import deque
+
+    e57 = pye57.E57(path)
+
+    path = Path(path)
+    extract_path = path.parent / f"{path.stem}_scene_info"
+    os.makedirs(extract_path, exist_ok=True)
+    os.makedirs(extract_path / "images", exist_ok=True)
+
+    # 1. Extract all cameras
+    cam_infos = []
+
+    for idx, image in tqdm(enumerate(e57.root["images2D"]), desc="Processing images", total = e57.scan_count * 6):
+        if (idx + 1) % 6 == 0 or (idx + 2) % 6 == 0: continue
+        # image_path = os.path.join(path, cam_name)
+        # image_name = Path(cam_name).stem
+        jpeg_buffer = image["pinholeRepresentation"]["jpegImage"].read_buffer()
+        image_data = Image.open(io.BytesIO(jpeg_buffer))
+        if not (extract_path / f"images/{idx}.jpg").exists():
+            with open(extract_path / f"images/{idx}.jpg", "wb") as fh:
+                fh.write(jpeg_buffer)
+
+        # Camera intrinsic matrix
+        focal_length = image["pinholeRepresentation"]["focalLength"].value() # Note: In meters, must be translated to pixels
+        image_width = image["pinholeRepresentation"]["imageWidth"].value()
+        image_height = image["pinholeRepresentation"]["imageHeight"].value()
+        pixel_width = image["pinholeRepresentation"]["pixelWidth"].value()
+        pixel_height = image["pinholeRepresentation"]["pixelHeight"].value()
+
+        FovX = focal2fov(focal_length / pixel_width, image_width)
+        FovY = focal2fov(focal_length / pixel_height, image_height)
+
+        R = quaternion.as_rotation_matrix(np.quaternion(
+            image["pose"]["rotation"]["w"].value(),
+            image["pose"]["rotation"]["x"].value(),
+            image["pose"]["rotation"]["y"].value(),
+            image["pose"]["rotation"]["z"].value(),
+        ))
+
+        T = np.array([
+            image["pose"]["translation"]["x"].value(),
+            image["pose"]["translation"]["y"].value(),
+            image["pose"]["translation"]["z"].value(),
+        ])
+
+        extrinsic = np.column_stack((R, T))
+        extrinsic = np.row_stack((extrinsic, [0, 0, 0, 1]))
+
+        # change from OpenGL/Blender camera axes (Y up, Z back) to COLMAP (Y down, Z forward)
+        extrinsic[:3, 1:3] *= -1
+
+        # Specify camera's pose directly, instead of how world is transformed around camera.
+        extrinsic = np.linalg.inv(extrinsic)
+
+        # R is stored transposed due to 'glm' in CUDA code
+        R = np.transpose(extrinsic[:3,:3])
+        T = extrinsic[:3, 3]
+
+        cam_infos.append(CameraInfo(
+            uid=idx,
+            R=R, T=T, FovX=FovX, FovY=FovY,
+            image=image_data, image_path=f"{extract_path}/images/{idx}.jpg", image_name=f"{idx}.jpg",
+            width=image_width, height=image_height))
+
+    # 2. Split cameras into train/test (?)
+    if eval:
+        train_cam_infos = [c for idx, c in enumerate(cam_infos) if idx % llffhold != 0]
+        test_cam_infos = [c for idx, c in enumerate(cam_infos) if idx % llffhold == 0]
+    else:
+        train_cam_infos = cam_infos
+        test_cam_infos = []
+
+    # 3. Just run getNerfppNorm?
+    nerf_normalization = getNerfppNorm(train_cam_infos)
+
+    # 4. Get pointcloud info in BasicPointCloud format
+    ply_path = extract_path / "pointcloud.ply"
+    pcd = o3d.geometry.PointCloud()
+    if not os.path.exists(ply_path):
+        points = deque()
+        colors = deque()
+        for idx in tqdm(range(e57.scan_count), desc="Assembling pointcloud"):
+            p, c = point_data(e57, idx)
+            points.append(p)
+            colors.append(c)
+
+        points = np.concatenate(points)
+        colors = np.concatenate(colors)
+
+        print("Simplifying pointcloud")
+        pcd.points = o3d.utility.Vector3dVector(points)
+        pcd.colors = o3d.utility.Vector3dVector(colors)
+
+        pcd = pcd.voxel_down_sample(voxel_size=0.1)
+
+        print("Caching pointcloud data")
+        points = np.asarray(pcd.points)
+        colors = np.asarray(pcd.colors)
+        pcd = BasicPointCloud(points, colors, normals=np.zeros((len(points), 3)))
+        storePly(ply_path, points, colors)
+
+    # pcd = o3d.io.read_point_cloud(str(ply_path))
+    # render_ply(pcd, train_cam_infos)
+    # sys.exit(0)
+
+    try:
+        pcd = fetchPly(ply_path)
+    except:
+        pcd = None
+
+    scene_info = SceneInfo(
+        point_cloud=pcd,
+        train_cameras=train_cam_infos,
+        test_cameras=test_cam_infos,
+        nerf_normalization=nerf_normalization,
+        ply_path=str(ply_path)
+    )
+
+    print("HELLO E57 <3")
+
+    return scene_info
+
+def render_ply(pcd, cameras):
+    """
+    pcd = o3d.io.read_point_cloud(str(ply_path))
+    render_ply(pcd, train_cam_infos)
+    """
+    from tqdm import tqdm
+    import open3d as o3d
+
+    pcd.colors = o3d.utility.Vector3dVector(np.asarray(pcd.colors) * 255)
+
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+    vis.add_geometry(pcd)
+    for cam_info in tqdm(cameras, desc="Adding cameras"):
+        intrinsic = o3d.camera.PinholeCameraIntrinsic(
+            cam_info.width, cam_info.height,
+            fov2focal(cam_info.FovX, cam_info.width),
+            fov2focal(cam_info.FovY, cam_info.height),
+            cam_info.width / 2, cam_info.height / 2
+        );
+
+        extrinsic = np.column_stack((cam_info.R, cam_info.T))
+        extrinsic = np.row_stack((extrinsic, [0, 0, 0, 1]))
+        extrinsic[:3,:3] = np.transpose(extrinsic[:3,:3])  # R is stored transposed due to 'glm' in CUDA code
+
+        camera = o3d.geometry.LineSet.create_camera_visualization(intrinsic, extrinsic);
+        vis.add_geometry(camera)
+
+    vis.run()
+    vis.destroy_window()
+
+def point_data(e57, index):
+    header = e57.get_header(index)
+    data = e57.read_scan(index, colors=True)
+
+    # Create pointcloud - Should be global
+    points = np.column_stack((data["cartesianX"], data["cartesianY"], data["cartesianZ"]))
+
+    try:
+        assert header["colorLimits"]["colorRedMaximum"].value() == 255
+        assert header["colorLimits"]["colorGreenMaximum"].value() == 255
+        assert header["colorLimits"]["colorBlueMaximum"].value() == 255.0
+    except:
+        pass
+
+    colors = np.column_stack((
+        np.array(data["colorRed"]),
+        np.array(data["colorGreen"]),
+        np.array(data["colorBlue"]),
+    ))
+
+    return points, colors
+
 sceneLoadTypeCallbacks = {
     "Colmap": readColmapSceneInfo,
-    "Blender" : readNerfSyntheticInfo
+    "Blender" : readNerfSyntheticInfo,
+    "E57": readE57SceneInfo,
 }
